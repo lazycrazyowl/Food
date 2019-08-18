2c78680b639fab96256fc95abaf65c25e32c638c
PeerChristensen <hr.pchristensen@gmail.com>
2019-08-18 13:57:38 +0200

    pairs

--- ingredients_EDA.R
+++ ingredients_EDA.R
@@ -10,9 +10,25 @@ df %>% unnest_tokens(word,ingredients_short) %>%
   count(word) %>%
   arrange(desc(n))
 
-df %>% unnest_tokens(word,ingredients_short) %>%
+pair_count <- df %>% 
+  unnest_tokens(word,ingredients_short) %>%
+  filter(!word %in% c("sale","olio","pepe")) %>%
   pairwise_count(word,dish_names) %>%
+  mutate(pair = glue::glue("{item1}, {item2}"))
+  
+pair_count$pair <- pair_count$pair %>%
+   str_split(", ") %>% 
+   map(sort) %>%
+   map(paste0,collapse=", ") %>% unlist()
+
+pair_count <- pair_count %>%
   arrange(desc(n)) %>%
-  mutate(row = row_number()) %>%
-  filter(row %% 2 != 0) %>%
-  select(-row)
+  distinct(pair,.keep_all = T) %>%
+  mutate(row = rev(row_number()))
+
+pair_count %>%
+  top_n(25,n) %>%
+  ggplot(aes(row,n)) +
+  geom_col() +
+  coord_flip() +
+  scale_x_continuous(breaks=pair_count$row,labels=pair_count$pair)
