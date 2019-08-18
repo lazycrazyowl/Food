34b912fb150309dc0038a437c4c907ad88324fac
PeerChristensen <hr.pchristensen@gmail.com>
2019-08-18 14:51:28 +0200

    add two plots

--- ingredients_EDA.R
+++ ingredients_EDA.R
@@ -26,9 +26,26 @@ pair_count <- pair_count %>%
   distinct(pair,.keep_all = T) %>%
   mutate(row = rev(row_number()))
 
-pair_count %>%
-  top_n(25,n) %>%
+pair_count_25 <- pair_count %>%
+  top_n(25,n) 
+
+pair_count_25 %>%
   ggplot(aes(row,n)) +
-  geom_col() +
+  geom_col(fill="darkred",width=.5) +
   coord_flip() +
-  scale_x_continuous(breaks=pair_count$row,labels=pair_count$pair)
+  scale_x_continuous(breaks=pair_count_25$row,labels=pair_count_25$pair) +
+  theme_minimal() +
+  theme(panel.grid.major.y = element_blank(),
+        panel.grid.minor.y = element_blank())
+
+pair_count %>%
+  select(item1,item2,n) %>%
+  filter(n > 2) %>%
+  graph_from_data_frame() %>%
+  ggraph(layout = "fr") +
+  geom_edge_link(aes(edge_alpha = n, edge_width = n), edge_colour = "darkred") +
+  geom_node_point(size = 5) +
+  geom_node_text(aes(label = name), repel = TRUE,
+                 point.padding = unit(0.2, "lines")) +
+  theme_void() +
+  theme(legend.position = "none")
